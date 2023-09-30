1c92cb32eb80d5ad58cdc260a8adea4c13ef5542
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-30 21:05:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+/*  your css code here. If applicable */
+// Get the element with the id 'level'
+var targetElement = document.getElementById('level');
+
+// Function to determine the DOM level of an element
+function getDomLevel(element) {
+    var level = 0;
+    while (element.parentNode) {
+        level++;
+        element = element.parentNode;
+    }
+    return level;
+}
+
+// Call the function and display the result using alert
+alert("The level of the element is: " + getDomLevel(targetElement));
